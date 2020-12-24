2afa1616d0ced82460165caf21c7beae4e4d8be4
Akshita Agarwal <akshita.ag321@gmail.com>
2020-12-24 14:12:06 +0530

    Added Team and Footer Components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,61 @@
 import React from "react";
-
+import './Footer.css';
+import logo from '../images/aichelogo.png';
 export default class Footer extends React.Component{
     render(){
         return(
-            <div clasName = "Footer">
-            <p>Footer</p>
-            </div>
+            <footer class="footer-distributed">
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"/>
+
+	<link href="https://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css"></link>
+                <div class="footer-left">
+
+                <img src={logo} id = 'aiche_logo'/>
+                <br/><br/>
+
+                <p class="footer-company-name">AIChE IITR &copy; 2020</p>
+                </div>
+
+                <div class="footer-center">
+
+                    <div>
+                        <i class="fa fa-map-marker"></i>
+                        <p><span>Roorkee - Haridwar Highway </span>Roorkee, Uttarakhand 247667</p>
+                    </div>
+
+                    <div>
+                        <i class="fa fa-phone"></i>
+                        <p>+1 555 123456</p>
+                    </div>
+
+                    <div>
+                        <i class="fa fa-envelope"></i>
+                        <p><a href="#">aiche.iitr@gmail.com</a></p>
+                    </div>
+
+                </div>
+
+                <div class="footer-right">
+
+                    <p class="footer-company-about">
+                        <span>ABOUT</span>
+                        The American Institute of Chemical Engineers Chapter | IIT Roorkee is a student run chemical engineering professional organization. </p>
+
+                    <div class="footer-icons">
+
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                        <a href="#"><i class="fa fa-envelope"></i></a>
+
+                    </div>
+
+                </div>
+                <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="https://cdn.tutorialzine.com/misc/enhance/v2.js" async></script>
+
+</footer>
         )
     }
 }
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,15 +1,155 @@
 import React from "react";
+import '../Team/Team.css';
 import Navbar from "../Navbar/Navbar.jsx";
-import Footer from "../Footer/Footer.jsx";
 export default class Team extends React.Component{
     render(){
         return(
             <div className = "Team">
+                
                 <Navbar active = {"Team"}/>
                 <br/>
                 <br/>
-                <h1>Team </h1>
-                <Footer/>
+                <center><h1 id = 'team'>TEAM</h1>
+                    <br/><br/>
+                <div class="container mt-50">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="box16">
+                                <img src="http://bestjquery.com/tutorial/hover-effect/demo118/images/img-1.jpg"/>
+                                <div class="box-content">
+                                    <h3 class="title">Williamson</h3>
+                                    <span class="post">Web Developer</span>
+                                    <ul class="social">
+                                        
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                                    </ul>
+                                </div>
+                        </div>
+                        </div>
+                        </div>
+                        <br/><br/><br/>
+                <div class="container mt-40">
+                    <div class="row mt-30">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="box16">
+                                <img src="http://bestjquery.com/tutorial/hover-effect/demo118/images/img-1.jpg"/>
+                                <div class="box-content">
+                                    <h3 class="title">Williamson</h3>
+                                    <span class="post">Web Developer</span>
+                                    <ul class="social">
+                                        
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="box16">
+                                <img src="http://bestjquery.com/tutorial/hover-effect/demo118/images/img-2.jpg"/>
+                                <div class="box-content">
+                                    <h3 class="title">Kristiana</h3>
+                                    <span class="post">Web Designer</span>
+                                    <ul class="social">
+                                        
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="box16">
+                                <img src="http://bestjquery.com/tutorial/hover-effect/demo118/images/img-3.jpg"/>
+                                <div class="box-content">
+                                    <h3 class="title">Kristiana</h3>
+                                    <span class="post">Web Designer</span>
+                                    <ul class="social">
+                                       
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="box16">
+                                <img src="http://bestjquery.com/tutorial/hover-effect/demo118/images/img-3.jpg"/>
+                                <div class="box-content">
+                                    <h3 class="title">Kristiana</h3>
+                                    <span class="post">Web Designer</span>
+                                    <ul class="social">
+                                        
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br/><br/><br/>
+                <div class="container mt-40">
+                    <div class="row mt-30">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="box16">
+                                <img src="http://bestjquery.com/tutorial/hover-effect/demo118/images/img-1.jpg"/>
+                                <div class="box-content">
+                                    <h3 class="title">Williamson</h3>
+                                    <span class="post">Web Developer</span>
+                                    <ul class="social">
+                                        
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="box16">
+                                <img src="http://bestjquery.com/tutorial/hover-effect/demo118/images/img-2.jpg"/>
+                                <div class="box-content">
+                                    <h3 class="title">Kristiana</h3>
+                                    <span class="post">Web Designer</span>
+                                    <ul class="social">
+                                        
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="box16">
+                                <img src="http://bestjquery.com/tutorial/hover-effect/demo118/images/img-3.jpg"/>
+                                <div class="box-content">
+                                    <h3 class="title">Kristiana</h3>
+                                    <span class="post">Web Designer</span>
+                                    <ul class="social">
+                                        
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="box16">
+                                <img src="http://bestjquery.com/tutorial/hover-effect/demo118/images/img-3.jpg"/>
+                                <div class="box-content">
+                                    <h3 class="title">Kristiana</h3>
+                                    <span class="post">Web Designer</span>
+                                    <ul class="social">
+                                        
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </center>
             </div>
         )
     }
