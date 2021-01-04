2b330ad13f7d21d6dcc10f3c1c6b8640224f732a
smrpn <samarpan_d@pp.iitr.ac.in>
2021-01-05 01:48:01 +0530

    added navbar and blogs

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,18 +1,110 @@
-import React from "react";
-import Navbar from "../Navbar/Navbar.jsx";
-import Footer from "../Footer/Footer.jsx";
-import "./Blogs.css";
-export default class Blogs extends React.Component{
-    render(){
-        return(
-            <div className = "Blogs">
-                <Navbar active = {"Blogs"}/>
-                <br/>
-                <br/>
-                <br/>
-                <h1>Blogs</h1>
-                <Footer/>
-            </div>
+import React, { Component } from 'react'
+import Navbar from '../Navbar/Navbar'
+import './blogs.css'
+
+
+
+class Blogs extends Component {
+    render() {
+
+
+        return (
+
+
+            <div>
+                <Navbar active={"Blogs"} />
+                <h1 className="heading">Blogs</h1>
+                <div id="cards_landscape_wrap-2">
+
+                    {/* 1st */}
+                    <div className="container-fluid">
+
+                        <div className="row" style={{ margin: "auto" }}>
+                            <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+
+                                <div className="card-flyer">
+                                    <div className="text-box">
+                                        <div className="image-box">
+                                            <img src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_960_720.jpg" alt="" />
+                                        </div>
+                                        <div className="text-container">
+                                            <h6>Title 01</h6>
+
+                                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy...<a className="readmore" href='blog1'>read more</a></p>
+                                            <p>By - Author</p>
+                                            <p>Date</p>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
+                            <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+                                <a href="">
+                                    <div className="card-flyer">
+                                        <div className="text-box">
+                                            <div className="image-box">
+                                                <img src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_960_720.jpg" alt="" />
+                                            </div>
+                                            <div className="text-container">
+                                                <h6>Title 01</h6>
+                                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+                                <a href="">
+                                    <div className="card-flyer">
+                                        <div className="text-box">
+                                            <div className="image-box" >
+                                                <img src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_960_720.jpg" alt="" />
+                                            </div>
+                                            <div className="text-container">
+                                                <h6>Title 01</h6>
+                                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+                            <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+                                <a href="">
+                                    <div className="card-flyer">
+                                        <div className="text-box">
+                                            <div className="image-box">
+                                                <img src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_960_720.jpg" alt="" />
+                                            </div>
+                                            <div className="text-container">
+                                                <h6>Title 01</h6>
+                                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
+
+                    {/* 2nd */}
+
+                </div>
+
+
+            </div >
         )
     }
 }
+
+export default Blogs
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,110 @@
-import React from "react";
-import "./Navbar.css";
-export default class Navbar extends React.Component{
-    render(){
-        return(
-          <div className = "Navbar">
-            <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-            <a class="navbar-brand" href="/"
-            > AIChe IIT Roorkee </a
-            >
-            <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-            <li class="nav-item hoverable">
-            <a class="nav-link" href="/one">one</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/two">two</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/three">three</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/four">four</a>
-          </li>             
-            </ul>
-        </div>
-      </nav>
-      </div>
+import React, { Component } from 'react'
+import aiche from '../../images/AIChe.png'
+
+
+
+
+class NavigationBar extends Component {
+
+    render() {
+        const routes = [
+            "Home",
+            "Blogs",
+            "Competitions",
+            "Team",
+            "Contact Us"
+        ]
+        // return (
+        //     <div>
+        //         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+        //             <Navbar.Brand>
+        //                 <img
+        //                     alt=""
+        //                     src={aiche}
+        //                     width="30"
+        //                     height="30"
+        //                     className="d-inline-block align-top"
+        //                 />{' '}
+        //                 <NavLink exact to="/" className={style.mainnav} >AIChE IITR</NavLink>
+        //             </Navbar.Brand>
+        //             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        //             <Navbar.Collapse id="responsive-navbar-nav">
+        //                 <Nav className="mr-auto" >
+        //                     {/* <Nav.Link><NavLink exact to="/blogs" className={style.mainnav} activeClassName={style.mainnavactive}>Blogs</NavLink></Nav.Link>
+        //                     <Nav.Link><NavLink exact to="/pricing" className={style.mainnav} activeClassName={style.mainnavactive}>About</NavLink></Nav.Link>
+        //                     <Nav.Link><NavLink exact to="/competitions" className={style.mainnav} activeClassName={style.mainnavactive}>Competitions</NavLink></Nav.Link>
+        //                     <Nav.Link><NavLink exact to="/contact-us" className={style.mainnav} activeClassName={style.mainnavactive}>Contact Us</NavLink></Nav.Link> */}
+
+        //                 </Nav>
+        //                 <Nav style={{ textAlign: "left" }}>
+
+        //                     <Nav.Link><NavLink exact to="/home" className={style.mainnav} activeClassName={style.mainnavactive}>Home</NavLink></Nav.Link>
+        //                     <Nav.Link><NavLink exact to="/blogs" className={style.mainnav} activeClassName={style.mainnavactive}>Blogs</NavLink></Nav.Link>
+        //                     <Nav.Link><NavLink exact to="/competitions" className={style.mainnav} activeClassName={style.mainnavactive}>Competitions</NavLink></Nav.Link>
+
+        //                     <Nav.Link><NavLink exact to="/team" className={style.mainnav} activeClassName={style.mainnavactive}>Team</NavLink></Nav.Link>
+        //                     <Nav.Link><NavLink exact to="/contact-us" className={style.mainnav} activeClassName={style.mainnavactive}>Contact Us</NavLink></Nav.Link>
+
+        //                 </Nav>
+        //             </Navbar.Collapse>
+        //         </Navbar>
+        //     </div>
+        // )
+
+        return (
+            <div className="Navbar">
+                <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+                    <a className="navbar-brand" href="/"
+                    >
+                        <img
+                            alt=""
+                            src={aiche}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />
+                    &nbsp;AIChe IIT Roorkee </a
+                    >
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            {routes.map(r => {
+                                if (this.props.active == r) {
+                                    return (
+                                        <li className="nav-item active">
+                                            <a class="nav-link" href={"/" + r}>{r}</a>
+                                        </li>
+                                    )
+
+                                }
+                                else {
+                                    return (
+                                        <li className="nav-item">
+                                            <a class="nav-link" href={"/" + r}>{r}</a>
+                                        </li>
+                                    )
+                                }
+                            })}
+
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
+
+export default NavigationBar
+
+
+
